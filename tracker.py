74f91e0a71345507eeca9d7161d83f8be407251f
Stephen Tanksley <stephen.tanksley@gmail.com>
2021-01-19 23:29:18 -0600

    feat: Updated functionality to allow users to define date ranges.

--- tracker.py
+++ tracker.py
@@ -13,35 +13,77 @@ st.write("""
          
          """)
 
-current_portfolio = {'F': 'Ford',
-                     'CLF': 'Cleveland-Cliffs',
-                     'CPRX': 'Catalyst Pharmaceuticals',
-                     'AMZN': 'Amazon',
-                     'TXMD': 'TherepeuticsMD'}
+# current_portfolio = {'F': 'Ford',
+#                      'CLF': 'Cleveland-Cliffs',
+#                      'CPRX': 'Catalyst Pharmaceuticals',
+#                      'AMZN': 'Amazon',
+#                      'TXMD': 'TherapeuticsMD'}
 
+current_portfolio = {'Ford': 'F',
+                     'Cleveland-Cliffs': 'CLF',
+                     'Catalyst Pharmaceuticals': 'CPRX',
+                     'Amazon': 'AMZN',
+                     'TherapeuticsMD': 'TXMD'}
 
-# Define the ticker we want to track.
-# ticker_symbol = random.choice(current_stocks)
+# date_period = {
+#     '1 day': '1d',
+#     '5 days': '5d',
+#     '1 month': '1mo',
+#     '3 months': '3mo',
+#     '6 months': '6mo',
+#     '1 year': '1y',
+#     '2 years': '2y',
+#     '5 years': '5y',
+#     '10 years': '10y',
+#     'Year to date': 'ytd',
+#     'Max': 'max'
+# }
 
-# for key, value in current_portfolio.items():
-#     print(f"{key}: {value}")
+selection = st.sidebar.selectbox("Which stock would you like to track?", ('Ford',
+                                                                          'Cleveland-Cliffs',
+                                                                          'Catalyst Pharmaceuticals',
+                                                                          'Amazon',
+                                                                          'TherapeuticsMD'))
 
-ticker_symbol = random.choice(list(current_portfolio.keys()))
-
-# Get the data attached to this ticker.
-ticker_data = yf.Ticker(ticker_symbol)
+# period = st.sidebar.selectbox("Which period resolution would you like to track?", ('1 day',
+#                                                                                    '5 days',
+#                                                                                    '1 month',
+#                                                                                    '3 months',
+#                                                                                    '6 months',
+#                                                                                    '1 year',
+#                                                                                    '2 years',
+#                                                                                    '5 years',
+#                                                                                    '10 years',
+#                                                                                    'Year to date',
+#                                                                                    'Max'))
 
 # Get current date
 today = date.today()
 
+# Allow users to define start and end dates
+start_date = st.sidebar.date_input(
+    "Please select a start date: ", date(2010, 1, 1))
+end_date = st.sidebar.date_input(
+    "Please select an end date: ", today)
+
+
+# Get the data attached to this ticker.
+ticker_data = yf.Ticker(current_portfolio[selection])
+
+
 # Get the historical prices for this ticker.
+
+# # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+
 ticker_dataframe = ticker_data.history(
-    period='1d', start='2010-5-31', end=today)
+    period='1d', start=start_date, end=end_date)
 
 # Open     High     Low Close    Volume    Dividends    Stock Splits
 
-st.write(f"Price of {current_portfolio[ticker_symbol]} at close.")
+price_string = f"### Price of {selection} ('**{current_portfolio[selection]}**') at close."
+st.markdown(price_string)
 st.line_chart(ticker_dataframe.Close)
 
-st.write(f" Volume of {current_portfolio[ticker_symbol]} at close. ")
+volume_string = f"### Volume Price of {selection} ('**{current_portfolio[selection]}**'). "
+st.markdown(volume_string)
 st.line_chart(ticker_dataframe.Volume)
